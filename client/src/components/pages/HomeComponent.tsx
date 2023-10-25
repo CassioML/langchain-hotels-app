8f391ae1bdf9e6601435157113ad21824614366a
Stefano Lottini <stefano.lottini@datastax.com>
2023-10-25 12:03:22 +0200

    app flow diagrams in-app slideshow

--- client/src/components/pages/HomeComponent.tsx
+++ client/src/components/pages/HomeComponent.tsx
@@ -14,18 +14,30 @@ import '../App.css';
 
 const slides = [
   "/slideshow/hotels.png",
-  "/slideshow/monopoli.png",
+  "/slideshow/flow1_review_summary.png",
+  "/slideshow/flow2_user_summary.png",
+  "/slideshow/flow3_user-specific_review_summary.png",
+  "/slideshow/flow4_real-time_new_review.png",
+  "/slideshow/flow5_llm_caching.png",
   "/slideshow/monopoli.png",
 ];
 const titles = [
   "Welcome",
-  "Diagram",
-  "Diagram",
+  "Generic hotel summary",
+  "User preferences summary",
+  "User-specific hotel summary",
+  "Posting a new review",
+  "LLM Caching",
+  "Thank you for watching",
 ]
 const descs = [
   "Your personalized and AI-powered hotel search. Built with LangChain and Astra DB.",
-  "Here a diagram (todo) ...",
-  "Another diagram (todo) ...",
+  "For each hotel, some of its reviews are condensed into a quick bullet-point summary.",
+  "Users can specify their preferences, which are then made into a user-summary string.",
+  "When looking at hotel details, a user-specific quick hotel description is created.",
+  "New review are made available to all the app flows in real-time.",
+  "A global (Astra DB-backed) cache for LLM prompt/response is quickly set up for great savings on tokens and latencies.",
+  "We hope you enjoyed this small slideshow.",
 ]
 
 const HomeComponent = () => {
